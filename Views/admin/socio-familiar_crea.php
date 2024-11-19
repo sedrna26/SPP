cb9a046822822cfd67cd381543d1132673af9cf7
Andres Rodriguez <sedrna26@gmail.com>
2024-11-19 20:44:44 -0300

    Update socio-familiar_crea.php

--- Views/admin/socio-familiar_crea.php
+++ Views/admin/socio-familiar_crea.php
@@ -267,21 +267,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['guardar_3'])) {
             </select>
             <input type="text" name="padre_instruccion" placeholder="Grado de instrucción/profesión/oficio"
                 required>
-            <div id="padre_details" class="familiar-container">
-                <input type="text" name="padre_apellido" placeholder="Apellido" required>
-                <input type="text" name="padre_nombre" placeholder="Nombre" required>
-                <input type="number" name="padre_edad" placeholder="Edad" required>
-                <input type="text" name="padre_nacionalidad" placeholder="Nacionalidad" required>
-                <select name="padre_estado_civil" required>
-                    <option value="">Estado Civil</option>
-                    <option value="Soltero">Soltero</option>
-                    <option value="Casado">Casado</option>
-                    <option value="Divorciado">Divorciado</option>
-                    <option value="Viudo">Viudo</option>
-                    <option value="Union Convivencial">Unión Convivencial</option>
-                </select>
-                <input type="text" name="padre_instruccion" placeholder="Grado de instrucción/profesión/oficio" required>
-            </div>
+
         </div>
 
         <!-- Datos de la Madre -->
